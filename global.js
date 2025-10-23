f50af48f1f9671d0bc9e7a37805fe490928d1231
Layth Marabeh <laythmarabeh@gmail.com>
2025-10-22 17:34:05 -0700

    update js files

--- global.js
+++ global.js
@@ -122,8 +122,12 @@ export async function fetchJSON(url) {
   try {
     // Fetch the JSON file from the given URL
     const response = await fetch(url);
+    if (!response.ok) {
+  throw new Error(`Failed to fetch projects: ${response.statusText}`);
+}
+    const data = await response.json();
+    return data;
   } catch (error) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
-
