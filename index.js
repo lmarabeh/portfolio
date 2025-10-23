fbb5e7267b0ce9c8df3604c2a2f9515beeab01b4
Layth Marabeh <laythmarabeh@gmail.com>
2025-10-22 21:13:12 -0700

    Lab 4 Step 2

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+import { fetchJSON, renderProjects} from './global.js';
+const projects = await fetchJSON('./lib/projects.json');
 
+async function loadProjects() {
+  // Get the first three projects
+  const latestProjects = projects.slice(0, 3);
+  
+  // Find the container on the page
+  const projectsContainer = document.querySelector('.projects_home');
+
+  // Check if we found the data and the container
+  if (latestProjects && projectsContainer) {
+    
+    // Clear the container 
+    projectsContainer.innerHTML = ''; 
+
+    // Loop through the projects
+    latestProjects.forEach(project => {
+      
+      // Call helper function for EACH project
+      renderProjects(project, projectsContainer, 'h2');
+    });
+    
+  } else {
+    console.error('Could not load projects or find container.');
+  }
+}
+
+// Call the main function to load projects when the page loads
+loadProjects();
